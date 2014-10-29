a2c471ce4c03910f41dfbf183693f11c63712535
AbelDz <abel.diaz.cardona@gmail.com>
2014-10-29 16:17:00 -0600

    CodeReviewr 1st release
    CodeReviewr 1st release

--- CodeReviewr.py
+++ CodeReviewr.py
@@ -5,71 +5,15 @@ __email__ = "abel.diaz.cardona@gmail.com"
 
 import sublime, sublime_plugin
 
-class CodeReviewrCommand(sublime_plugin.TextCommand):
-
-	trace = CodeTrace("CodeTrace") #TO DO: in the future it will support many Code Traces at the same time
-
-	def __init__(self,view):
-		self.window = None
-		super(CodeReviewrCommand,self).__init__(view)
-		
-	def run(self,edit,mode="toggle"):
-		self.window = self.view.window()
-		output_panel = self.window.create_output_panel("CodeReveiwrLog")
-		output_panel.set_read_only(False)
-
-		if mode == "toggle":
-			self.view.run_command("toggle_bookmark")		
-			self.window.show_input_panel("Enter a label for the line >> ","",self.on_input_addlabel_done,None,None)
-			#output_panel.insert(edit,output_panel.size(),"Line successfully added to trace")
-
-		elif mode == "search":
-			keys = CodeReviewrCommand.trace.get_formatted_keys()
-			self.window.show_quick_panel(keys,self.on_line_select_done)
-			output_panel.insert(edit,output_panel.size(),"Line successfully added to CodeReviewr")
-
-		elif mode == "clear":
-			CodeReviewrCommand.trace = None
-			CodeReviewrCommand.trace = CodeTrace("CodeTrace")
-			self.view.run_command("clear_bookmarks")
-			output_panel.insert(edit,output_panel.size(),"CodeReviewr buffer cleared")
-			#output_panel.insert(edit,output_panel.size(),str(row+1)) #output test line
-			self.display_codereviewr_output()
-
-	def on_input_addlabel_done(self, label): #generate CodeLine objects instead of just text
-		path = self.view.file_name()
-		position = self.view.sel()[0].begin()
-		code = self.view.substr(self.view.line(position))
-		(row,col) = self.view.rowcol(position)
-		code_line = CodeLine(label,path,position,row+1,code)
-		CodeReviewrCommand.trace.add(code_line)
-
-	def on_line_select_done(self,selection): #once selected go to the line in the file
-		selIndex = int(selection)
-		selected_code_line = CodeReviewrCommand.trace.get_by_index(selIndex)
-		file_to_open = str(selected_code_line.get_file_path())
-		line_to_go = selected_code_line.get_line_no()
-
-		file_view = self.window.open_file(file_to_open)
-		while(file_view.is_loading()):
-			pass
-
-		file_view.run_command("goto_line", {"line": line_to_go} )
-		
-	def display_codereviewr_output(self):
-		self.window.run_command("show_panel",{"panel": "output.CodeReveiwrLog"})
-
-
-
-
-class CodeLine(): #a CodeTrace will be conformed of a list of this data structure
+class CodeLine: #a CodeTrace will be conformed of a list of this data structure
 	
-	def __init__(self,label,file_path,file_position,line_no,code):
+	def __init__(self,label,file_path,file_position,line_no,code,physical_file):
 		self.label = label
 		self.file_path = file_path
 		self.file_position = file_position
 		self.lineNo = line_no
 		self.code = code
+		self.physical_file = physical_file
 
 	def get_label(self):
 		return self.label
@@ -86,10 +30,10 @@ class CodeLine(): #a CodeTrace will be conformed of a list of this data structur
 	def get_file_position(self):
 		return self.file_position
 
+	def is_in_physical_file(self):
+		return self.physical_file
 
-
-
-class CodeTrace():
+class CodeTrace:
 	
 	def __init__(self,name):
 		self.name = name
@@ -101,13 +45,84 @@ class CodeTrace():
 	def get_by_index(self,index):
 		return self.code_lines[index]
 
-	def get_formatted_keys(self): #this method will return the string array to populate the quick selection panel when searching within the trace
+	def get_formatted_keys(self): #this method will return the string array to populate the quick selection panel when searching within the CodeTrace
 		keys = []
 		for code_line in self.code_lines:
 			label = str(code_line.get_label())
 			line_no = str(code_line.get_line_no())
 			code = str(code_line.get_code().strip()[:100])
-			filename = str(code_line.get_file_path()[code_line.get_file_path().rfind("\\"):]) #TO DO: will only work for windows, fix for the rest of the platforms
+			filename = "BufferedFile"
+			if code_line.is_in_physical_file():
+				file_path = code_line.get_file_path()
+				path_separator = "/"
+				if "\\" in file_path: #if it's a Windows path
+					path_separator = "\\"
+				filename = str(file_path[file_path.rfind(path_separator):]) 
 			keys.append("\"" + label + "\" - FILE: " + filename + " LINE: " +line_no + ". " + code)
 		return keys
+
+class CodeReviewrCommand(sublime_plugin.TextCommand):
+
+	trace = CodeTrace("CodeTrace") #TO DO: in the future it will support many Code Traces at the same time
+
+	def __init__(self,view):
+		self.window = None
+		super(CodeReviewrCommand,self).__init__(view)
 		
+	def run(self,edit,mode="toggle"):
+		self.window = self.view.window()
+		output_panel = self.window.create_output_panel("CodeReveiwrLog")
+		output_panel.set_read_only(False)
+
+		if mode == "toggle":
+			self.view.run_command("toggle_bookmark")		
+			self.window.show_input_panel("Enter a label for the line >> ","",self.on_input_addlabel_done,None,None)
+			output_panel.insert(edit,output_panel.size(),"Line successfully added to trace")
+
+		elif mode == "search":
+			keys = CodeReviewrCommand.trace.get_formatted_keys()
+			self.window.show_quick_panel(keys,self.on_line_select_done)
+			output_panel.insert(edit,output_panel.size(),"Line successfully added to CodeReviewr")
+
+		elif mode == "clear":
+			CodeReviewrCommand.trace = None
+			CodeReviewrCommand.trace = CodeTrace("CodeTrace")
+			self.view.run_command("clear_bookmarks")
+			output_panel.insert(edit,output_panel.size(),"CodeReviewr buffer cleared")
+			self.display_codereviewr_output()
+
+	def on_input_addlabel_done(self, label): #Once the label is entered, create a CodeLine object and add it to the CodeTrace
+		path = self.view.file_name()
+		physical_file = True
+
+		if path == None: #file is just buffered, use View object instead
+			path = self.view
+			physical_file = False
+
+		position = self.view.sel()[0].begin()
+		code = self.view.substr(self.view.line(position))
+		(row,col) = self.view.rowcol(position)
+		code_line = CodeLine(label,path,position,row+1,code,physical_file)
+		CodeReviewrCommand.trace.add(code_line)
+
+	def on_line_select_done(self,selection): #once selected go to the line in the file
+		selIndex = int(selection)
+		selected_code_line = CodeReviewrCommand.trace.get_by_index(selIndex)
+		line_to_go = selected_code_line.get_line_no()
+		file_to_open = selected_code_line.get_file_path()
+		is_physical_file = selected_code_line.is_in_physical_file()
+		file_view = None
+		
+		if is_physical_file: #if it's a physical file
+			file_view = self.window.open_file(file_to_open)
+			while(file_view.is_loading()):
+				pass
+		else:				 #if it's only a buffered file
+			file_view = file_to_open
+			self.window.focus_view(file_view)
+
+		file_view.run_command("goto_line", {"line": line_to_go} )
+		
+	def display_codereviewr_output(self):
+		self.window.run_command("show_panel",{"panel": "output.CodeReveiwrLog"})
+
